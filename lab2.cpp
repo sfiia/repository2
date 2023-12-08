14c71e6b4a0736839a9b318a7378f35bc601bc8a
sfiia <150463021+sfiia@users.noreply.github.com>
2023-12-09 00:51:44 +0300

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -40,6 +40,7 @@ int rightBorder(int arr[], int value, int low, int mid, int high) {
     return low;
 }
 
+
 int leftBorder(int arr[], int value, int low, int mid, int high) {
     while ((high - low) > 1) {
         mid = (low + high) / 2;
@@ -53,7 +54,7 @@ int leftBorder(int arr[], int value, int low, int mid, int high) {
     return high;
 }
 
-int elementFinder(int arr[], int n, int value) {
+int findElement(int arr[], int n, int value) {
     for (int i = 0; i < n; ++i) {
         if (value == arr[i]) {
             return 1;
@@ -62,7 +63,6 @@ int elementFinder(int arr[], int n, int value) {
     return 0;
 
 }
-
 int binSearch(int arr[], int value, const int len) {
     int low = 0;
     int high = len - 1;
@@ -81,7 +81,26 @@ int binSearch(int arr[], int value, const int len) {
     }
     return -1;
 }
+
+void idz9(int arr[], int len, int num) {
+    for (int i = 1; i < len; i += 2) {
+        if (i % 2) {
+            arr[i] -= num;
+            arr[i] *= rand() % 9 + 1;
+        }
+    }
+    for (int j = 1; j < 10; ++j) {
+        cout << "Элементы, кратные " << j << ": ";
+        for (int i = 0; i < len; ++i) {
+            if (!(arr[i] % j)) {
+                cout << arr[i] << " ";
+            }
+        }
+        cout << "\n";
+    }
+}
 //-----------------------------------------------------------------------
+
 void randomMassive(int arr[], const int n)
 {
     setlocale(LC_ALL, "ru");
@@ -102,6 +121,7 @@ void randomMassive(int arr[], const int n)
     start = steady_clock::now();
     int mx = arr[0];
     int mn = arr[0];
+
     for (int i = 1; i < n; ++i) {
         mx = max(mx, arr[i]);
         mn = min(mn, arr[i]);
@@ -113,11 +133,14 @@ void randomMassive(int arr[], const int n)
     cout << "Поиск max и min в неотсортированном массиве заняла " << result.count() << " наносекунд\n";
     quickSort(arr, 0, n - 1);
 }
+
+
 // ----------------------------------------------------------------------
 void sortedMassive(int arr[], const int n){
     int a,b;
     int avg;
     int index;
+    int problem;
     int low, mid = 0, high, l_border, r_border;
     int index1, index2;
     time_mngr start, end;
@@ -127,18 +150,12 @@ void sortedMassive(int arr[], const int n){
     cout<< "Введите значение а, чтобы посмотреть количество элементов\n";
     cout<< "в отсортированном массиве, которые МЕНЬШЕ этого числа :\n";
     cin >> a;
-    cin.sync();
-    cin.clear();
     cout<< "Введите значение b, чтобы посмотреть количество элементов\n";
     cout<< "в отсортированном массиве, которые БОЛЬШЕ этого числа :\n";
     cin >> b;
-    cin.sync();
-    cin.clear();
     cout <<"";
     cout << "Вводите индексы элементов(от 0 до 99), которые хотите поменять местами (Вводить попарно и через пробел):\n";
     cin >> index1 >> index2;
-    cin.sync();
-    cin.clear();
     //2 TASK
     start = steady_clock::now();
     quickSort(arr, 0, n - 1);
@@ -157,11 +174,12 @@ void sortedMassive(int arr[], const int n){
     int mn =arr[0];
     end = steady_clock::now();
     result = end - start;
-    cout << "\nmin: " << mn << ", max: " << mx;
-    cout << "\nПоиск max и min значений отсортированного массива занял " << result.count() << " наносекунд\n";
+    cout << "min: " << mn << ", max: " << mx;
+    cout << "\n";
+    cout << "Поиск max и min значений отсортированного массива занял " << result.count() << " наносекунд\n";
     //4 TASK
     avg = round((mx + mn) / 2.0);
-    cout << "\nCРЕДНЕЕ ЗНАЧЕНИЕ максимального и минимального значения :" << avg<< "\n";
+    cout << "Cреднее значение максимального и минимального значения :" << avg<< "\n";
     if (binSearch(arr, avg, n) == -1) {
         cout << "Такого элемента нет в массиве";
     }
@@ -177,6 +195,8 @@ void sortedMassive(int arr[], const int n){
     }
     cout << "\n";
     //5 TASK
+    cin.sync();
+    cin.clear();
     low = 0;
     high = n - 1;
     l_border = leftBorder(arr, a, low, mid, high);
@@ -186,10 +206,9 @@ void sortedMassive(int arr[], const int n){
     high = n - 1;
     r_border = rightBorder(arr, b, low, mid, high);
     cout << n - 1 - r_border << " элементов, больших " << b << "\n";
-    cout<< "\n";
     //7 TASK
     start = steady_clock::now();
-    elementFinder(arr, n, a);
+    findElement(arr, n, a);
     end = steady_clock::now();
     result = end - start;
     cout << "Обычный перебор прошел за " << result.count() << " наносекунд\n";
@@ -209,14 +228,15 @@ void sortedMassive(int arr[], const int n){
         start = steady_clock::now();
         swap(arr[index1], arr[index2]);
         end = steady_clock::now();
-        cout <<"\nМассив с индексами, которые поменяли местами :";
-        for (int i = 1; i < n; ++i) {
-            cout << " " << arr[i];
-        }
-        cout << "\nЗамена прошла за " << (end - start).count() << " наносекунд\n";
+        cout << "Замена прошла за " << (end - start).count() << " наносекунд\n";
         cin >> index1 >> index2;
     }
+    for (int i = 0; i < n; ++i) {
+        arr[i] = rand() % 200 - 99;
+    }
 }
+
+
  //---------------------------------------------------------------------------
 int main(){
     const int n = 100;
